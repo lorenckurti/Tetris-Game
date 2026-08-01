724026ffb946839fa4099fd43c742348d0328752
Apple <apple@Apples-MacBook-Pro.local>
2026-08-01 21:49:59 +0200

    fix pmj4 url

---
+++ dist/assets/gaussianSplatting.fragment-C3yApkDm.js
@@ -0,0 +1,28 @@
+import{t as e}from"./shaderStore-D-XQlhUT.js";import{D as t,O as n,P as r,U as i,W as a,a as o,dt as s,k as c}from"./index-D1Cgq-bk.js";var l=`gaussianSplattingPixelShader`,u=`#include<clipPlaneFragmentDeclaration>
+#include<logDepthDeclaration>
+#include<fogFragmentDeclaration>
+#ifdef GPUPICKER_PACK_DEPTH
+#include<packingFunctions>
+#endif
+varying vColor: vec4f;varying vPosition: vec2f;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+#include<gaussianSplattingFragmentDeclaration>
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+var finalColor: vec4f=gaussianColor(input.vColor,input.vPosition);
+#define CUSTOM_FRAGMENT_BEFORE_FRAGCOLOR
+#ifdef GPUPICKER_DEPTH
+fragmentOutputs.fragData0=finalColor;
+#ifdef GPUPICKER_PACK_DEPTH
+fragmentOutputs.fragData1=pack(fragmentInputs.position.z);
+#else
+fragmentOutputs.fragData1=vec4f(fragmentInputs.position.z,0.0,0.0,1.0);
+#endif
+#else
+fragmentOutputs.color=finalColor;
+#endif
+#define CUSTOM_FRAGMENT_MAIN_END
+}
+`;e.ShadersStoreWGSL[l]||(e.ShadersStoreWGSL[l]=u);var d=[a,r,c,s,n,t,o,i];for(let t of d)e.IncludesShadersStoreWGSL[t.name]||(e.IncludesShadersStoreWGSL[t.name]=t.shader);var f={name:l,shader:u};export{f as gaussianSplattingPixelShaderWGSL};
