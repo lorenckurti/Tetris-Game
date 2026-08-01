4b0135cf2f56a89d8133b2e979c3378dd8166f8c
Apple <apple@Apples-MacBook-Pro.local>
2026-08-01 21:53:06 +0200

    fix server url

--- dist/assets/volumetricLightingRenderVolume.vertex-BDxyzRAG.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./shaderStore-D-XQlhUT.js";import{t}from"./sceneUboDeclaration-B5VhSG0v.js";import{t as n}from"./meshUboDeclaration-BmNu2KU_.js";import{B as r,z as i}from"./index-D1Cgq-bk.js";var a=`volumetricLightingRenderVolumeVertexShader`,o=`#include<__decl__sceneVertex>
-#include<__decl__meshVertex>
-attribute vec3 position;varying vec4 vWorldPos;void main(void) {vec4 worldPos=world*vec4(position,1.0);vWorldPos=worldPos;gl_Position=viewProjection*worldPos;}
-`;e.ShadersStore[a]||(e.ShadersStore[a]=o);var s=[r,t,i,n];for(let t of s)e.IncludesShadersStore[t.name]||(e.IncludesShadersStore[t.name]=t.shader);var c={name:a,shader:o};export{c as volumetricLightingRenderVolumeVertexShader};
